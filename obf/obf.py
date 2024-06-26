6d2dbab4921e71063262f2e470d8e1eedcff4305
43a1723 <hai1723notgay@gmail.com>
2024-06-26 09:36:10 +0700

    Update obf.py

--- obf/obf.py
+++ obf/obf.py
@@ -3,6 +3,6 @@ import numpy as np
 from obf.obfvar import *
 
 def obfuscate(code):
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789%"
     code = obfuscate(code, alphabet, 8, 5)
     return code
