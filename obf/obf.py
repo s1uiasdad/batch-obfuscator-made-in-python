a9149ad32d136172c5b8b38ae021264fac4cafe3
43a1723 <hai1723notgay@gmail.com>
2024-06-26 09:33:38 +0700

    Update obf.py

--- obf/obf.py
+++ obf/obf.py
@@ -1,4 +1,7 @@
+import random
+import numpy as np
+
 def obfuscate(code):
-    """docstring"""
-    # function body
-    return codeobf
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    code = obfuscate(code, alphabet, 8, 5)
+    return code
